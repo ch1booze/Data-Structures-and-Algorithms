65b35b58a9187138a044c8f08ca5757ca7851757
Victor Momodu <victoramomodu@gmail.com>
2024-05-20 22:32:10 +0100

    feat: Recoded Dijstra's algorithm using Grokking

--- vector_graph.py
+++ vector_graph.py
@@ -1,116 +1,61 @@
-from collections import deque
 import math
 
 
 class VectorGraph:
     def __init__(self) -> None:
-        self.connections = dict()
-        self.weights = dict()
+        self.graph = dict()
+
+    def does_node_exist(self, node):
+        return self.graph.get(node, False)
 
     def add_edge(self, src, dst, weight):
-        does_node_exist = self.connections.get(src, False)
-
-        if not does_node_exist:
-            self.connections[src] = list()
-            self.weights[src] = list()
-            self.connections[src].append(dst)
-            self.weights[src].append(weight)
-            print(f"Added a path between {src} and {dst} with a distance {weight}.")
-
+        if not self.does_node_exist(src):
+            self.graph[src] = {dst: weight}
         else:
-            if not dst in self.connections[src]:
-                self.connections[src].append(dst)
-                self.weights[src].append(weight)
-                print(f"Added a path between {src} and {dst} with a distance {weight}.")
+            self.graph[src][dst] = weight
 
-            else:
-                print(f"A path already exists between {src} and {dst}.")
+        if not self.does_node_exist(dst):
+            self.graph[dst] = dict()
 
-    def remove_edge(self, src, dst):
-        does_node_exist = self.connections.get(src, False)
+    def dijkstra(self, start):
+        def find_lowest_cost_node(costs, processed):
+            lowest_cost = math.inf
+            lowest_cost_node = None
+            for node in costs:
+                cost = costs[node]
+                if cost < lowest_cost and node not in processed:
+                    lowest_cost = cost
+                    lowest_cost_node = node
+            return lowest_cost_node
 
-        if does_node_exist:
-            if dst in self.connections[src]:
-                index = self.connections[src].index(dst)
-                self.connections[src].remove(dst)
-                self.weights[src].pop(index)
-                print(f"Removed path between {src} and {dst}.")
+        nodes_except_start = [node for node in self.graph.keys() if node != start]
+        costs = {node: math.inf for node in nodes_except_start}
+        parents = {node: None for node in nodes_except_start}
+        processed = list()
 
-                if not self.connection[src]:
-                    del self.connections[src]
-                    del self.weights[src]
-                    print(f"{src} is not longer a source.")
+        start_neighbors = self.graph[start].keys()
+        for node in start_neighbors:
+            costs[node] = start
+            parents[node] = self.graph[start][node]
 
-        else:
-            print(f"{src} is not a source.")
+        current_node = find_lowest_cost_node(costs)
+        while current_node is not None:
+            cost = costs[current_node]
+            neighbors = self.graph[current_node]
 
-    def change_weight(self, src, dst, new_weight):
-        does_node_exist = self.connections.get(src, False)
-
-        if does_node_exist:
-            if dst in self.connections[src]:
-                index = self.connections[src].index(dst)
-                self.weights[src][index] = new_weight
-                print(f"Distance between {src} and {dst} is now {new_weight}.")
-
-            else:
-                print(f"There is no path between {src} and {dst}.")
-
-        else:
-            print(f"{src} is not a source.")
-
-    def get_nodes(self):
-        nodes = list()
-        for node_list in self.connections.values():
-            nodes += node_list
-        nodes = list(set(nodes))
-        return nodes
-
-    def get_sources(self):
-        return set(self.connections.keys())
+            for n in neighbors.keys():
+                new_cost = cost + neighbors[n]
+                if costs[n] > new_cost:
+                    costs[n] = new_cost
+                    parents[n] = current_node
+            processed.append(current_node)
+            current_node = find_lowest_cost_node(costs, processed)
 
 
 if __name__ == "__main__":
-    vectors = VectorGraph()
-
-    vectors.add_edge("Book", "LP", 5)
-    vectors.add_edge("Book", "Poster", 0)
-    vectors.add_edge("LP", "Guitar", 15)
-    vectors.add_edge("LP", "Drum", 20)
-    vectors.add_edge("Poster", "Guitar", 30)
-    vectors.add_edge("Poster", "Drum", 35)
-    vectors.add_edge("Guitar", "Piano", 20)
-    vectors.add_edge("Drum", "Piano", 10)
-
-    nodes = vectors.get_nodes()
-    sources = vectors.get_sources()
-    start_node = "Book"
-    end_node = "Piano"
-    dijkstra_distances = {node: math.inf for node in nodes if node != start_node}
-    dijkstra_parents = {node: None for node in nodes if node != start_node}
-    current_node = start_node
-    processed_nodes = set()
-    q = deque()
-
-    while sources.intersection(processed_nodes) != sources:
-        distances = vectors.weights[current_node]
-        connections = vectors.connections[current_node]
-
-        shortest_distance = min(distances)
-        index_for_shortest_dist = distances.index(shortest_distance)
-        node_for_shortest_dist = connections[index_for_shortest_dist]
-
-        if shortest_distance < dijkstra_distances[node_for_shortest_dist]:
-            dijkstra_distances[node_for_shortest_dist] = shortest_distance
-            dijkstra_parents[node_for_shortest_dist] = current_node
-
-        for node in connections:
-            if node in sources:
-                q.append(node)
-
-        processed_nodes.add(current_node)
-        current_node = q.pop()
-
-    print(dijkstra_distances)
-    print()
-    print(dijkstra_parents)
+    v = VectorGraph()
+    v.add_edge("start", "a", 6)
+    v.add_edge("start", "b", 2)
+    v.add_edge("a", "fin", 1)
+    v.add_edge("b", "a", 3)
+    v.add_edge("b", "fin", 1)
